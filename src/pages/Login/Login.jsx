2dbbfc9a012abe506d58d15cc8f361d20c00a819
antubhatta <antubhatta09@gmail.com>
2023-10-05 22:58:45 +0600

    dragon news recap

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h3 className="text-3xl">This is a login page</h3>
+        </div>
+    );
+};
+
+export default Login;
