477cfbe980ac91f80e15fcf17700ccf1fb649617
antubhatta <antubhatta09@gmail.com>
2023-10-06 12:20:09 +0600

    news load added successfully

---
+++ src/pages/News/News.jsx
@@ -0,0 +1,13 @@
+import Header from "../Shared/Header/Header";
+
+
+const News = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h3 className="text-3xl">News details page</h3>
+        </div>
+    );
+};
+
+export default News;
